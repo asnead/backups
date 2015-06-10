9f2ea68a3fbccb61fe127ccac543c7b77683703c
asnead <andrew.snead@atos.net>
2015-06-10 16:05:58 +0100

    first commit

---
+++ backup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+backupdir=$HOME/Atos/Backups;
+
+day=`date +%u`;
+mkdir -p $backupdir/$day;
+
+echo "PostgreSQL Backup for $HOSTNAME";
+echo "================================="
+echo `date`;
+for database in `/usr/local/bin/psql -U postgres -lt | awk '{print $1}' | grep -vE '\||^$|template|postgres'`;
+    do
+    printf "Exporting $database...";
+    /usr/local/bin/pg_dump -U postgres | gzip -c > $backupdir/$day/$database.sql.gz;
+    /bin/chmod 600 $backupdir/$day/$database.sql.gz;    
+    printf "done\n";
+done
+cd $backupdir;
+git add -A && git commit -m "new database backup for " + date && git push;
+echo `date`;
+echo;
+exit 0;
